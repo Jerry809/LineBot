e2105e9deb0f1743a0de2cbbcfea39d279737b61
Jerry <horizon809@gmail.com>
2018-07-10 11:21:02 +0800

    add eat random

--- BeanChat/Controllers/EatController.cs
+++ BeanChat/Controllers/EatController.cs
@@ -6,24 +6,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace BeanChat.Controllers
 {
     public class EatController : ApiController
     {
-        // GET: api/Eat
         [Route("~/api/eat/{area}")]
-        public async Task<EatModel> Get(string area)
+        public async Task<List<TemplateModel>> Get(string area)
         {
             var eat = new Eat();
-            return await eat.GetEatData(area);
-        }
-
-        public async Task<List<TemplateModel>> Get()
-        {
-            var eat = new Eat();
-            var data = await eat.GetEatData("三重");
+            var data = await eat.GetEatData(area);
 
             var list = new List<TemplateModel>();
 
@@ -41,49 +35,18 @@ namespace BeanChat.Controllers
                     defaultAction = new UriModel()
                     {
                         label = "瀏覽網誌",
-                        uri = item.url
+                        uri = HttpUtility.UrlDecode(item.url)
                     },
                     actions = new List<UriModel>() {
                             new UriModel()
                             {
                                 label = "導航",
-                                uri =  $"https://www.google.com.tw/maps/place/{item.lat},{item.lng}"
+                                uri =  $"https://www.google.com.tw/maps/place/{item.address}"
                             }
                         }
                 });
             }
             list.Add(model);
-
-            //var list = new List<TemplateModel>();
-
-            //var model = new TemplateModel();
-            //model.template = new CarouselModel();
-            //model.template.columns = new List<ThumbnailImageModel>();
-            //model.template.columns.Add(new ThumbnailImageModel()
-            //{
-            //    thumbnailImageUrl = "https://lh3.googleusercontent.com/4b4HA5R94a4xeljMuskixfjJvuhcvRyKcqqLM-_ouckm4zp_lxXEMR3H6UyofG0xnEnEv_WHfwupbK_5AQ5XkaunHFfHIB3J=s600",
-            //    title = "title",
-            //    text = "text",
-            //    defaultAction = new UriModel()
-            //    {
-            //        label = "打開地圖",
-            //        uri = "https://www.google.com.tw/maps/place/23.230344,120.353937"
-            //    },
-            //    actions = new List<UriModel>() {
-            //                new UriModel()
-            //                {
-            //                    label = "打開地圖",
-            //                    uri = "https://www.google.com.tw/maps/place/23.230344,120.353937"
-            //                },
-            //                 new UriModel()
-            //                {
-            //                    label = "瀏覽網誌",
-            //                    uri = "https://www.google.com.tw/maps/place/23.230344,120.353937"
-            //                }
-            //            }
-            //});
-
-            //list.Add(model);
             return list;
         }
     }
--- BeanChat/Module/Eat/Eat.cs
+++ BeanChat/Module/Eat/Eat.cs
@@ -11,7 +11,7 @@ namespace BeanChat.Module
 {
     public class Eat
     {
-        private const string url = "https://ifoodie.tw/api/blog/?order_by=-date&q={0}";
+        private const string url = "https://ifoodie.tw/api/blog/?offset={0}&limit={1}&order_by=-date&q={2}";
         private HttpClient Client;
 
         public Eat()
@@ -21,9 +21,22 @@ namespace BeanChat.Module
 
         public async Task<EatModel> GetEatData(string area)
         {
-            var data = await Client.GetAsync(string.Format(url, HttpUtility.UrlEncode(area)));
-            var content = await data.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<EatModel>(content);
+            EatModel result = null;
+            var random = new Random().Next(0, 10);
+            int offset = random * 20;
+            int limit = 20;
+            int count = 0;
+
+            while (count == 0 && offset > 0)
+            {
+                var data = await Client.GetAsync(string.Format(url, offset, limit, HttpUtility.UrlEncode(area)));
+                var content = await data.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<EatModel>(content);
+                count = result.response.Count;
+                offset -= 20;
+            }
+
+            return result;
         }
 
     }
